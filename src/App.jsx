e8bedea2edf25d7fb9f9621ef27ff188b1cc044a
INDRAJITPAAL <indrajitpal389@gmail.com>
2025-07-17 23:03:45 +0545

    react count mount unmount concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { useEffect, useMemo, useState } from "react";
+import { use } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [visibility, setVisiblity] = useState(true);
+
+  useEffect(() => {
+    let clock = setInterval(() => {
+      setVisiblity((prev) => !prev);
+    }, 5000);
+    return function () {
+      console.log("unMount in visility")
+      clearInterval(clock);
+    }
+
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>hii there start</h1>
+      {visibility && <Counter />}
+      <h1>hii there last</h1>
     </>
   )
 }
+
+
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  console.log("count");
+  useEffect(() => {
+    let clock = setInterval(() => {
+      console.log("counter interval");
+      setCount(prev => prev + 1);
+    }, 1000)
+
+    return function () {
+      console.log("unMount in counter")
+      clearInterval(clock);
+    }
+
+  }, []);
+
+  return (
+    <>
+
+      <h1> count is :  {count}</h1>
+
+    </>
+  )
+}
+
+
 
 export default App
